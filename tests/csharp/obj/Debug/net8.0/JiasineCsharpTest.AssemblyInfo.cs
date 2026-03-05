e7ea52cf1fa93c49f601eb556404242d77e1e890
xiangjianhe <xiangjianhe@cvte.com>
2026-03-05 17:37:16 +0800

    cs

--- tests/csharp/obj/Debug/net8.0/JiasineCsharpTest.AssemblyInfo.cs
+++ tests/csharp/obj/Debug/net8.0/JiasineCsharpTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JiasineCsharpTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed5b64f0393a411a7a5797af7a8da015ee59046")]
 [assembly: System.Reflection.AssemblyProductAttribute("JiasineCsharpTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JiasineCsharpTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
